6087b0ae88f54068713b1a2ef7cf94d6a8126dab
Nathan Thanakone <nathan.thanakone@gmail.com>
2023-04-20 12:40:03 -0400

    added api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [riddles, setRiddles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/riddles')
+      .then((response) => {
+        setRiddles(response.data);
+        console.log(riddles);
+      })
+  }, []);
+
   return (
     <div className="App">
-         </div>
+    </div>
   );
 }
 
